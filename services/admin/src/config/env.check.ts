5c616f5985af6a2da749a851d950e06a2413f1ba
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2024-11-25 13:04:46 +0530

    patch : gave return type for functoin

--- services/admin/src/config/env.check.ts
+++ services/admin/src/config/env.check.ts
@@ -1,6 +1,6 @@
 import { EnvChecker } from "@mb-medibook/common"
 
-export const envChecker = () => {
+export const envChecker = () : void => {
     EnvChecker(process.env.PORT as string, 'PORT')
     EnvChecker(process.env.MONGO_URL as string, 'MONGO_URL')
     console.log('All env varaibles are defined');   
