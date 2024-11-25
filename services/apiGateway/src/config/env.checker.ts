c12af29a04d0c778654648ec430b4c916fa360e8
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2024-11-25 13:15:23 +0530

    patch : gave return type for func in apigateway

--- services/apiGateway/src/config/env.checker.ts
+++ services/apiGateway/src/config/env.checker.ts
@@ -1,6 +1,6 @@
 import { EnvChecker } from "@mb-medibook/common";
 
-export const envChecker = () => {
+export const envChecker = (): void => {
     EnvChecker(process.env.PORT as string, "PORT");
     EnvChecker(process.env.ACCESS_TOKEN_SECRET as string, "ACCESS_TOKEN_SECRET");
     EnvChecker(process.env.REFRESH_TOKEN_SECRET as string, "REFRESH_TOKEN_SECRET");
