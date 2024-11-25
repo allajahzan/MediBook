a3002f91ab9176470b5280dc3e3584389acc67a1
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2024-11-25 11:12:26 +0530

    patch : added env checking for jwt secretes

--- services/user/src/config/env.check.ts
+++ services/user/src/config/env.check.ts
@@ -2,5 +2,8 @@ import { EnvChecker } from "@mb-medibook/common"
 
 export const envChecker = () => {
     EnvChecker(process.env.PORT as string, 'PORT')
-    EnvChecker(process.env.MONGO_URL as string, 'MONGO_URL') 
+    EnvChecker(process.env.MONGO_URL as string, 'MONGO_URL')
+    EnvChecker(process.env.ACCESS_TOKEN_SECRET as string, 'ACCESS_TOKEN_SECRET')
+    EnvChecker(process.env.REFRESH_TOKEN_SECRET as string, 'REFRESH_TOKEN_SECRET')
+    console.log('All env varaibles are defined');   
 }
--- services/user/src/config/rabbitmq.ts
+++ services/user/src/config/rabbitmq.ts
@@ -24,7 +24,7 @@ class RabbitMQ {
                 this.connect();
             });
         } catch (err: any) {
-            console.log(err.message);
+            throw new Error(err)
         }
     }
 }
