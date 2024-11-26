7b22ff3180b70b75e0a7d2a1359718abf4054c48
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2024-11-26 20:39:30 +0530

    feat: added profile create consomer

--- services/user/src/server.ts
+++ services/user/src/server.ts
@@ -4,6 +4,7 @@ import { MongoDBConnection } from "@mb-medibook/common";
 import { envChecker } from "./config/env.check";
 import { rabbitmq } from "./config/rabbitmq";
 import { UserStatusConsumer } from "./app/messaging/consumer/user.status";
+import { DoctorProfileCreatedConsumer } from "./app/messaging/consumer/docProfile.created";
 
 // dotenv config
 dotenv.config();
@@ -15,7 +16,8 @@ const startServer = async (): Promise<void> => {
 
         // rabbimq connection
         await rabbitmq.connect();
-        new UserStatusConsumer().consume()
+        new UserStatusConsumer().consume();
+        new DoctorProfileCreatedConsumer().consume();
 
         // mongodb connecion
         await MongoDBConnection(process.env.MONGO_URL as string);
